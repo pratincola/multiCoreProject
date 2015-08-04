5afa3fe4f75b5fed572b6de9985b0f1bdd3e5fb9
pburman <pburman@cccis.com>
2015-08-03 19:41:30 -0500

    Added LockFreeQueue as per Michel & Scott. Could not figure out the problem with the other implementation LockFreeQueue

--- src/hw4/q1/LockFreeQueue.java
+++ src/hw4/q1/LockFreeQueue.java
@@ -1,102 +1,78 @@
 package hw4.q1;
 
-import edu.multicore.queues.MyQueue;
 
+import edu.multicore.queues.MyQueue;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class LockFreeQueue<T> implements MyQueue<T> {
-    private AtomicStampedReference<LockFreeQueue.Node<T>> head, tail;
+    private AtomicReference<LockFreeQueue.Node> head, tail;
 
     public LockFreeQueue(){
-        LockFreeQueue.Node<T> dummy = new LockFreeQueue.Node(null, null);
-        head = new AtomicStampedReference<Node<T>>(dummy, 0);
-        tail = new AtomicStampedReference<Node<T>>(dummy, 0);
+        LockFreeQueue.Node dummy = new LockFreeQueue.Node(null);
+        head = new AtomicReference<LockFreeQueue.Node>(dummy);
+        tail = new AtomicReference<LockFreeQueue.Node>(dummy);
     }
 
 
+
+    @Override
     public boolean enq(T value) {
-        Node<T> new_element = new Node<>(value, null);
-        // Keep trying until enqueue is done
-        Node<T> local_tail;
-        int[] stampHolder;
-        Node<T> next_element;
-        while(true){
-            local_tail = tail.getReference();
-            stampHolder = new int[1];
-            next_element = local_tail.next.get(stampHolder);
 
-            if(local_tail == tail.getReference()){
-                //Tail pointing to last node?
-                if(next_element == null){
-                    if(tail.getReference().next.compareAndSet(next_element, new_element, stampHolder[0], stampHolder[0] + 1))
-                        break;
+        Node node = new Node(value);
+
+        while(true){
+            Node last = tail.get();
+            Node next = last.next.get();
+            if(last == tail.get()){
+                if(next == null){
+                    if(last.next.compareAndSet(next, node)){
+                        tail.compareAndSet(last, node);
+                        return true;
+                    }
                 }
-                //Tail was not pointing to last node, so try to swing it over
-                else
-                    // ------------------------------Possible bug...---------------------------------------------
-                    tail.compareAndSet(local_tail, next_element, next_element.next.getStamp() , tail.getStamp() + 1);
+                else{
+                    tail.compareAndSet(last, next);
+                }
             }
         }
-        //After a successful enqueue, swing tail to the inserted node
-        // ------------------------------Possible bug...---------------------------------------------
-        boolean b = tail.compareAndSet(local_tail, new_element, stampHolder[0], tail.getStamp() + 1);
-        System.out.println("changing tail pointer: " + b);
-        return true;
     }
 
+    @Override
     public T deq() {
-        //Keep trying until dequeue is done
-        T return_val;
-        Node<T> local_head;
-        Node<T> local_tail;
-        Node<T> next_element;
+
         while(true){
-            local_head = head.getReference();
-            local_tail = tail.getReference();
-            next_element = local_head.next.getReference();
-            //Are head, tail, and next consistent?
-            if (local_head == head.getReference()){
-                //Is queue empty or tail falling behind?
-                if (local_head == local_tail){
-                    //Is queue empty?
-                    if (next_element == null) {
-                        return null;
+            Node first = head.get();
+            Node last = tail.get();
+            Node next = first.next.get();
+            if(first == head.get()){
+                if(first == last){
+                    if(next == null){
+                      return null;
                     }
-                    //try to advance tail since it is lagging
-                    // ------------------------------Possible bug...---------------------------------------------
-                    tail.compareAndSet(local_tail,next_element, next_element.next.getStamp(), tail.getStamp()+1);
+                    tail.compareAndSet(last, next);
                 }
-                //You are ready for dequeue operation
-                else {
-                    //Read value before atomic CAS operation, else
-                    //someone else will free the next node
-                    return_val = next_element.item;
-                    //Try to swing head to the next node, and complete the dequeue
-                    // ------------------------------Possible bug...---------------------------------------------
-                    if (head.compareAndSet(local_head, next_element, next_element.next.getStamp(), head.getStamp() + 1)){
-                        break;
+                else{
+                    T value = next.value;
+                    if(head.compareAndSet(first, next)){
+                        return value;
                     }
+
                 }
             }
         }
-        return return_val;
     }
 
+    public class Node {
 
+        public T value;
+        public AtomicReference<Node> next;
 
-    private static class Node <T> {
-        final T item;
-        volatile AtomicStampedReference<Node<T>> next;
-
-        public Node(T item, LockFreeQueue.Node<T> next) {
-            this.item = item;
-            this.next = new AtomicStampedReference<Node<T>>(next,0);
+        public Node(T value) {
+            this.value = value;
+            this.next = new AtomicReference<Node>(null);
         }
     }
-
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         LockFreeQueue l = new LockFreeQueue();
         System.out.println(l.head);
         System.out.println(l.tail);
@@ -104,7 +80,14 @@ public class LockFreeQueue<T> implements MyQueue<T> {
         System.out.println(l.enq(6));
         System.out.println(l.enq(7));
         System.out.println(l.enq(8));
+        System.out.println(l.deq());
+        System.out.println(l.deq());
         System.out.println(l.enq(9));
         System.out.println(l.enq(10));
+        System.out.println(l.deq());
+        System.out.println(l.deq());
+        System.out.println(l.deq());
+
     }
+
 }
---
+++ src/hw4/q1/LockFreeQueue2.java
@@ -0,0 +1,121 @@
+package hw4.q1;
+
+import edu.multicore.queues.MyQueue;
+
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+public class LockFreeQueue2<T> implements MyQueue<T> {
+    private AtomicStampedReference<LockFreeQueue2.Node<T>> head, tail;
+
+    public LockFreeQueue2(){
+        LockFreeQueue2.Node<T> dummy = new LockFreeQueue2.Node(null, null);
+        head = new AtomicStampedReference<Node<T>>(dummy, 0);
+        tail = new AtomicStampedReference<Node<T>>(dummy, 0);
+    }
+
+    public void seek(){
+        Node<T> h = head.getReference();
+        while(h != null){
+            System.out.println("Seek: " + h.item);
+            h = h.next.getReference();
+        }
+    }
+
+    public boolean enq(T value) {
+        Node<T> new_element = new Node<>(value, null);
+        // Keep trying until enqueue is done
+        Node<T> local_tail;
+        int[] stampHolder;
+        Node<T> next_element;
+        while(true){
+            local_tail = tail.getReference();
+            stampHolder = new int[1];
+            next_element = local_tail.next.getReference();
+
+            if(local_tail == tail.getReference()){
+                //Tail pointing to last node?
+                if(next_element == null){
+                    if(tail.getReference().next.compareAndSet(next_element, new_element, stampHolder[0], stampHolder[0] + 1))
+                        break;
+                }
+                //Tail was not pointing to last node, so try to swing it over
+                else
+                    // ------------------------------Possible bug...---------------------------------------------
+                    tail.compareAndSet(local_tail, next_element, next_element.next.getStamp() , tail.getStamp() + 1);
+            }
+        }
+        //After a successful enqueue, swing tail to the inserted node
+        // ------------------------------Possible bug...---------------------------------------------
+        boolean b = tail.compareAndSet(local_tail, new_element, stampHolder[0], tail.getStamp() + 1);
+        System.out.println("changing tail pointer: " + b);
+        return true;
+    }
+
+    public T deq() {
+        //Keep trying until dequeue is done
+        T return_val;
+        Node<T> local_head;
+        Node<T> local_tail;
+        Node<T> next_element;
+        while(true){
+            local_head = head.getReference();
+            local_tail = tail.getReference();
+            next_element = local_head.next.getReference();
+            //Are head, tail, and next consistent?
+            if (local_head == head.getReference()){
+                //Is queue empty or tail falling behind?
+                if (local_head == local_tail){
+                    //Is queue empty?
+                    if (next_element == null) {
+                        return null;
+                    }
+                    //try to advance tail since it is lagging
+                    // ------------------------------Possible bug...---------------------------------------------
+                    tail.compareAndSet(local_tail,next_element, next_element.next.getStamp(), tail.getStamp()+1);
+                }
+                //You are ready for dequeue operation
+                else {
+                    //Read value before atomic CAS operation, else
+                    //someone else will free the next node
+                    return_val = next_element.item;
+                    //Try to swing head to the next node, and complete the dequeue
+                    // ------------------------------Possible bug...---------------------------------------------
+                    if (head.compareAndSet(local_head, next_element, next_element.next.getStamp(), head.getStamp() + 1)){
+                        break;
+                    }
+                }
+            }
+        }
+        return return_val;
+    }
+
+
+
+    private static class Node <T> {
+        final T item;
+        volatile AtomicStampedReference<Node<T>> next;
+
+        public Node(T item, LockFreeQueue2.Node<T> next) {
+            this.item = item;
+            this.next = new AtomicStampedReference<Node<T>>(next,0);
+        }
+    }
+
+
+    public static void main(String[] args){
+        LockFreeQueue2 l = new LockFreeQueue2();
+        System.out.println(l.head);
+        System.out.println(l.tail);
+        l.seek();
+        System.out.println(l.enq(5));
+        System.out.println(l.enq(6));
+
+        l.seek();
+
+//
+//        System.out.println(l.enq(8));
+//        System.out.println(l.enq(9));
+//        System.out.println(l.enq(10));
+//        l.seek();
+    }
+}
